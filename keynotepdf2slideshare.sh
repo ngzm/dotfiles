0a0cb8c84c6abd3204b272677af2d82be065118a
naoki-nagazumi <naoki.nagazumi@ntt-at.co.jp>
2017-06-28 13:00:51 +0900

    iオプションがないsedに対応

--- keynotepdf2slideshare.sh
+++ keynotepdf2slideshare.sh
@@ -8,9 +8,9 @@ function keynotepdf2slideshare() {
   fi
 
   if [ $# -eq 1 ]; then
-    bash -c "LANG=C LC_ALL=C sed -i 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $1"
+    bash -c "LANG=C LC_ALL=C sed -e 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $1 > ${1}.new"
   else
-    echo "usage: convert_uploadable_keynote_pdf pdf_path" 1>&2
+    echo "usage: keynotepdf2slideshare pdf_path" 1>&2
 
     return 1
   fi

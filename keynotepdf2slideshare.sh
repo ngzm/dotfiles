30e978bbb3feb7a066dc08c3c02aee34b1c9c507
Naoki Nagazumi <nk.ngzm@gmail.com>
2017-07-01 00:16:21 +0900

    refine codes

--- keynotepdf2slideshare.sh
+++ keynotepdf2slideshare.sh
@@ -1,17 +1,14 @@
 function keynotepdf2slideshare() {
-  extension=${1##*.}
-
-  if [ $extension != "pdf" ]; then
-    echo 'Suffix needs to "pdf"' 1>&2
-
-    return 1
-  fi
-
-  if [ $# -eq 1 ]; then
-    bash -c "LANG=C LC_ALL=C sed -e 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $1 > ${1}.new"
-  else
+  if [ $# -ne 1 ]; then
     echo "usage: keynotepdf2slideshare pdf_path" 1>&2
-
     return 1
   fi
+
+  arg=$1
+  if [ ${arg##*.} != "pdf" ]; then
+    echo 'no match of file extension' 1>&2
+    return 1
+  fi
+
+  bash -c "LANG=C LC_ALL=C sed -e 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $arg > ${arg}.new"
 }
